cb8c4d0f62c9b7caf0d0d3af853f4c7538bd2cd5
Katiasun <katjatolok3@gmail.com>
2024-04-16 16:34:16 +0200

    write code for file Main

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,13 @@ export default function Navbar(props) {
     <nav className={props.darkMode ? "dark" : ""}>
       <h3 className="nav-logo_text">ReactFacts</h3>
 
-      <<div
-                className="toggler"
-            >
-                <p className="toggler--light">Light</p>
-                <div
-                    className="toggler--slider"
-                    onClick={props.toggleDarkMode}
-                >
-                    <div className="toggler--slider--circle"></div>
-                </div>
-                <p className="toggler--dark">Dark</p>
-            </div>
+      <div className="toggler">
+        <p className="toggler--light">Light</p>
+        <div className="toggler--slider" onClick={props.toggleDarkMode}>
+          <div className="toggler--slider--circle"></div>
+        </div>
+        <p className="toggler--dark">Dark</p>
+      </div>
     </nav>
   );
 }
